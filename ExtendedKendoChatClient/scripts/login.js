60e67d3edd0b06f6f09309ae5c4e4f9fecd7078b
Miroslav Ivanov <MdotIvanov@gmail.com>
2014-09-30 10:35:21 -0500

    Changes made by Miroslav Ivanov (MdotIvanov@gmail.com).

--- ExtendedKendoChatClient/scripts/login.js
+++ ExtendedKendoChatClient/scripts/login.js
@@ -52,6 +52,23 @@ app.viewmodels = app.viewmodels || {};
                     navigator.notification.alert(JSON.stringify(error), function () {}, "Registration failed", 'OK');
                     console.log(error);
                 });
+        },
+        checkConnection: function() {
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN] = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI] = 'WiFi connection';
+            states[Connection.CELL_2G] = 'Cell 2G connection';
+            states[Connection.CELL_3G] = 'Cell 3G connection';
+            states[Connection.CELL_4G] = 'Cell 4G connection';
+            states[Connection.CELL] = 'Cell generic connection';
+            states[Connection.NONE] = 'No network connection';
+            
+            var connectionType = "<strong>Connection type:</strong> " + states[networkState];
+            
+            return connectionType;
         }
 
     });
